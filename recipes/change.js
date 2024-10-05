ffbb03087fe44fcdf624767388ddbb5c8b19ace7
arminbhk <armin.hessari2012@gmail.com>
2024-10-05 10:57:01 +0200

    Delete recipes directory

--- recipes/change.js
+++
@@ -1,6 +0,0 @@
-const chooselang = document.getElementById('sublang');
-function MakeLabelSwed() {
-    document.getElementById('f1').innerHTML = "Sigma"
-}
-addEventListener(chooselang, click)
-if (chooselang.click) MakeLabelSwed();
